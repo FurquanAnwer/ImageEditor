5b57cd829fd50e9e5d8d3ca7a157516e82ef0cfc
Furquan Anwer <furquan.anwer@gmail.com>
2025-10-09 19:11:29 +0530

    added actions

---
+++ src/actions/users.tsx
@@ -0,0 +1,17 @@
+import { auth } from "../lib/auth";
+import {headers} from "next/headers"
+import { db } from "../server/db";
+
+export async function getUserCredits(){
+    const session = await auth.api.getSession({
+        headers : await headers(),
+    });
+    if(!session)return null;
+
+    const user = await db.user.findUniqueorThrow({
+        where : {id:session.user.id},
+        select:{credits:true},
+    });
+    return user.credits;
+}
+
